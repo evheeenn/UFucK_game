6ad5b3cc6ce8e33a12477825c3031e957c1f939c
evheeenn <nermalord@gmail.com>
2023-02-16 19:17:44 +0200

    Version 1.0

---
+++ script.js
@@ -0,0 +1,23 @@
+const fizik = document.getElementById("fizik");
+const yama = document.getElementById("yama");
+
+document.addEventListener("click", function(event) {
+	jump()
+});
+
+function jump () {
+	if (fizik.classList != "jump") {
+		fizik.classList.add("jump")
+	}
+	setTimeout( function() {
+		fizik.classList.remove("jump")
+	}, 300)
+}  
+	let isAlive = setInterval (function() {
+		let fizikTop = parseInt(window.getComputedStyle(fizik).getPropertyValue("top"));
+		let yamaLeft = parseInt(window.getComputedStyle(yama).getPropertyValue("left"));
+
+		if (yamaLeft < 42 && yamaLeft > 0 && fizikTop >= 100) {
+			alert("Гру завершено!")
+		}
+	}, 10)
